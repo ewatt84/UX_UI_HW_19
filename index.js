a14d61a844ec19b58c1c3bbd694925baf4381e18
ewatt84 <88289422+ewatt84@users.noreply.github.com>
2022-03-11 22:57:00 -0600

    js file

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Your index.js file is loaded correctly!") ;
+<script> 
+$(document).ready(function(){
+  $("button").click(function(){
+    $("#Erin's-microphone").hide();
+  })
+});
+</script>
